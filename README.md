2c89efe6648218e3848c01c00a5c1b9d0a9db83e
午默 <45330971+cn-wumo@users.noreply.github.com>
2021-04-29 23:30:50 +0800

    nacos build

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Investment-SpringCloud-2.4.5
-Investment-SpringCloud with SpringBoot2.4.5
+1.使用Nacos技术而非Eureka技术
+#所使用到的框架
+1.SpringBoot 2.4.5
+
+2.SpringCloud  2020.0.2
+
+3.Nacos 2021.1
---
+++ nacos-server/src/main/java/investment/NacosServerApplication.java
@@ -0,0 +1,31 @@
+package investment;
+
+import cn.hutool.core.net.NetUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosServerApplication {
+    public static void main(String[] args) {
+        int port = 8848;
+        if(!NetUtil.isUsableLocalPort(port)) {
+            System.err.printf("端口%d被占用了，无法启动%n", port );
+            System.exit(1);
+        }
+        SpringApplication.run(NacosServerApplication.class,args);
+    }
+
+    @RestController
+    static class EchoController {
+        @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+        public String echo(@PathVariable String string) {
+            return "Hello Nacos Discovery " + string;
+        }
+    }
+}
